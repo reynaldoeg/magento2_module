84d9e478245ceba394008c924c84caebf6310a2d
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2019-01-18 11:53:56 -0600

    InstallSchema: Crear Tabla

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tutorial\Example\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+
+        // Get tutorial_example table
+        $tableName = $installer->getTable('tutorial_example');
+        // Check if the table already exists
+        if ($installer->getConnection()->isTableExists($tableName) != true) {
+            // Create tutorial_example table
+            $table = $installer->getConnection()
+                ->newTable($tableName)
+                ->addColumn(
+                    'id',
+                    Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'ID'
+                )
+                ->addColumn(
+                    'title',
+                    Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false, 'default' => ''],
+                    'Title'
+                )
+                ->addColumn(
+                    'summary',
+                    Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false, 'default' => ''],
+                    'Summary'
+                )
+                ->addColumn(
+                    'description',
+                    Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false, 'default' => ''],
+                    'Description'
+                )
+                ->addColumn(
+                    'created_at',
+                    Table::TYPE_DATETIME,
+                    null,
+                    ['nullable' => false],
+                    'Created At'
+                )
+                ->addColumn(
+                    'status',
+                    Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false, 'default' => '0'],
+                    'Status'
+                )
+                ->setComment('News Table')
+                ->setOption('type', 'InnoDB')
+                ->setOption('charset', 'utf8');
+            $installer->getConnection()->createTable($table);
+        }
+
+        $installer->endSetup();
+    }
+}
