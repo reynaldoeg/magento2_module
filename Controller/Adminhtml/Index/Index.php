0772734dbe019d35f46d9969aeed1c9896b0b0f1
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2019-03-15 17:50:42 -0600

    Admin: rutas y controladores

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tutorial\Example\Controller\Adminhtml\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->prepend((__('Modulo Example')));
+
+        return $resultPage;
+    }
+}
