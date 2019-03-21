70b609b7a3156373f71ebe68be2e37ac0cfe501c
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2019-03-21 14:02:16 -0600

    MassActions

---
+++ Controller/Adminhtml/Item/Delete.php
@@ -0,0 +1,55 @@
+<?php
+namespace Tutorial\Example\Controller\Adminhtml\Item;
+ 
+use Magento\Backend\App\Action;
+ 
+class Delete extends Action
+{
+    protected $_model;
+ 
+    /**
+     * @param Action\Context $context
+     * @param \Tutorial\Example\Model\Item $model
+     */
+    public function __construct(
+        Action\Context $context,
+        \Tutorial\Example\Model\Item $model
+    ) {
+        parent::__construct($context);
+        $this->_model = $model;
+    }
+ 
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Tutorial_Example::item_delete');
+    }
+ 
+    /**
+     * Delete action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $id = $this->getRequest()->getParam('id');
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($id) {
+            try {
+                $model = $this->_model;
+                $model->load($id);
+                $model->delete();
+                $this->messageManager->addSuccess(__('Item deleted'));
+                return $resultRedirect->setPath('example/index/index');
+            } catch (\Exception $e) {
+                $this->messageManager->addError($e->getMessage());
+                return $resultRedirect->setPath('*/*/edit', ['id' => $id]);
+            }
+        }
+        $this->messageManager->addError(__('Item does not exist'));
+        return $resultRedirect->setPath('*/*/');
+    }
+}
